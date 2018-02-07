6a8917e21db45fc74eeaced2c0035c4074b8e670
adamscmRH <chadams@redhat.com>
2018-02-07 09:09:22 -0500

    prep for override AT

--- awx/api/urls/oauth.py
+++ awx/api/urls/oauth.py
@@ -7,9 +7,30 @@ from oauth2_provider.urls import base_urlpatterns
 
 from awx.api.views import (
     ApiOAuthAuthorizationRootView,
+    UserMeOauthApplicationList,
+    UserMeOauthApplicationDetail,
+    UserMeOauthApplicationTokenList,
+    UserMeOauthApplicationActivityStreamList,
 )
 
+
 urls = [
+    url(r'^applications/$', UserMeOauthApplicationList.as_view(), name='user_me_oauth_application_list'),
+    url(
+        r'^applications/(?P<pk>[0-9]+)/$',
+        UserMeOauthApplicationDetail.as_view(),
+        name='user_me_oauth_application_detail'
+    ),
+    url(
+        r'^applications/(?P<pk>[0-9]+)/tokens/$',
+        UserMeOauthApplicationTokenList.as_view(),
+        name='user_me_oauth_application_token_list'
+    ),
+    url(
+        r'^applications/(?P<pk>[0-9]+)/activity_stream/$',
+        UserMeOauthApplicationActivityStreamList.as_view(),
+        name='user_me_oauth_application_activity_stream_list'
+    ),
     url(r'^$', ApiOAuthAuthorizationRootView.as_view(), name='oauth_authorization_root_view'),
 ] + base_urlpatterns
 
